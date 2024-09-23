4b0f8e51c22928067d04214218d7f80ec26cb31d
Aviv Elany <132253879+AvivElany@users.noreply.github.com>
2024-09-23 11:05:27 +0300

    Delete server directory

--- server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { myProfile, mustLogin } = require('../controllers/authControllers');
-
-  //  base path = "/auth"
-  
-  router.get('/profile', mustLogin, myProfile)
-
-module.exports = router;
