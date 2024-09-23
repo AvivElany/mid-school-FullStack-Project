4b433ec4880f7b600c2fde8edf45f955f464d3c0
Aviv Elany <132253879+AvivElany@users.noreply.github.com>
2024-09-23 11:05:09 +0300

    Delete client directory

--- client/src/config.ts
+++
@@ -1,9 +0,0 @@
-
-export const appName:string = 'school-menagement'
-export const developer:string = 'Aviv Elany'
-export const lecturer:string = 'Elran Amrussi'
-export const classYear:number = 2024
-
-// ---------------------------------------------------------------------------------------------------------
-
-export const apiBase: string = 'http://127.0.0.1:3000'
